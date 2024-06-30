84d494af9403cfd5d03d6364469a3aee0e9c615b
Denis Kharitonov <manonov004@gmail.com>
2024-06-30 06:18:08 +0300

    all other stuff

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from pathways import Pathways
+from locators import MainLocator, LoginLocator
+from test_data import (mocky)
+
+
+@pytest.fixture
+def driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--incognito")
+    driver = webdriver.Chrome(options=options)
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture
+def mocky_auth_driver(driver):
+    driver.get(Pathways.base_path)
+    driver.find_element(*MainLocator.login_button).click()
+    driver.find_element(*LoginLocator.email_field).send_keys(mocky.email)
+    driver.find_element(*LoginLocator.pass_field).send_keys(mocky.password)
+    driver.find_element(*LoginLocator.login_button).click()
