c656d770beaf6ce4518794e49f6657d8a05f692b
Denis Kharitonov <manonov004@gmail.com>
2024-06-30 06:31:24 +0300

    Some shenanigans

--- pathways.py
+++
@@ -1,6 +0,0 @@
-class Pathways:
-    base_path = "https://stellarburgers.nomoreparties.site/" ## Головняк
-    reg_path = base_path+"register" ## Рега
-    login_path = base_path+"login" ## Вход
-    recover_path = base_path+"forgot-password" ## Сброс пасса
-    dash_path = base_path+"account/profile" ## Личный кабинет
